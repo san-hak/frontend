4384c637322a9e7be3dd51a3746b0871f6209229
smile0902 <rladjstn@dgsw.hs.kr>
2024-04-16 17:08:16 +0900

    :tada:
    프로젝트 구조 생성

---
+++ i-mirror/src/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello World</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
