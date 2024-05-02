19a075cda1ee45677484fc6e80d885554161e72e
smile0902 <rladjstn@dgsw.hs.kr>
2024-05-02 20:02:43 +0900

    :memo:
    검사 결과 조회

--- i-mirror/src/pages/result_list_inquiry/ResultListInquiry.jsx
+++ i-mirror/src/pages/result_list_inquiry/ResultListInquiry.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import * as T from "./resultListInquiryStyle";
+import iMirrorLogo from "../../asset/img/i-mirror_logo.svg";
 
 function ResultListInquiry() {
   return (
     <>
-      <div></div>
+      <div>
+        <img src={iMirrorLogo} alt="logo" />
+        <div>
+          <div>검색어를 입력해주세요</div>
+          <img src="" alt="검색" />
+        </div>
+      </div>
     </>
   );
 }
