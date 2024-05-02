b840e903ff7da2d0b63ab95acb7ea600ed2397e5
smile0902 <rladjstn@dgsw.hs.kr>
2024-05-02 19:51:24 +0900

    :wrench:

--- i-mirror/src/Router.js
+++ i-mirror/src/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ResultListInquiry from "./pages/result_list_inquiry/ResultListInquiry";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Hello World</h1>} />
+        <Route path="/main" element={<ResultListInquiry />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ i-mirror/src/pages/result_list_inquiry/ResultListInquiry.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as T from "./resultListInquiryStyle";
+
+function ResultListInquiry() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default ResultListInquiry;
