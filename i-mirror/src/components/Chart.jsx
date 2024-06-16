cc4b2308268e60359703c646c201435be799fcf7
smile0902 <rladjstn@dgsw.hs.kr>
2024-06-16 18:29:07 +0900

    :memo:

--- i-mirror/src/components/Chart.jsx
+++ i-mirror/src/components/Chart.jsx
@@ -1,9 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-import { patientList } from '../constant/patientList';
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { patientList } from "../constant/patientList";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Chart = ({ name, birth }) => {
   const [charts, setCharts] = useState([]);
@@ -12,7 +29,8 @@ const Chart = ({ name, birth }) => {
     const fetchData = () => {
       try {
         const data = patientList.filter(
-          patient => patient.koreanName === name && patient.birthDate === birth
+          (patient) =>
+            patient.koreanName === name && patient.birthDate === birth
         );
 
         console.log("Filtered Data:", data);
@@ -23,52 +41,54 @@ const Chart = ({ name, birth }) => {
         }
 
         const keys = [
-          "sagittalNeckUpperAngle", 
-          "sagittalNeckLowerAngle", 
-          "frontalNeckLeftAngle", 
+          "sagittalNeckUpperAngle",
+          "sagittalNeckLowerAngle",
+          "frontalNeckLeftAngle",
           "frontalNeckRightAngle",
-          "horizontalLeftArmUpperAngle", 
-          "horizontalLeftArmLowerAngle", 
-          "horizontalRightArmUpperAngle", 
+          "horizontalLeftArmUpperAngle",
+          "horizontalLeftArmLowerAngle",
+          "horizontalRightArmUpperAngle",
           "horizontalRightArmLowerAngle",
-          "sagittalLeftArmFrontAngle", 
-          "sagittalLeftArmRearAngle", 
-          "sagittalRightArmFrontAngle", 
+          "sagittalLeftArmFrontAngle",
+          "sagittalLeftArmRearAngle",
+          "sagittalRightArmFrontAngle",
           "sagittalRightArmRearAngle",
-          "frontalLeftArmOuterAngle", 
-          "frontalLeftArmInnerAngle", 
-          "frontalRightArmOuterAngle", 
+          "frontalLeftArmOuterAngle",
+          "frontalLeftArmInnerAngle",
+          "frontalRightArmOuterAngle",
           "frontalRightArmInnerAngle",
-          "sagittalWaistRearAngle", 
-          "sagittalWaistFrontAngle", 
-          "frontalWaistLeftAngle", 
+          "sagittalWaistRearAngle",
+          "sagittalWaistFrontAngle",
+          "frontalWaistLeftAngle",
           "frontalWaistRightAngle",
-          "leftKneeRearAngle", 
-          "rightKneeRearAngle"
+          "leftKneeRearAngle",
+          "rightKneeRearAngle",
         ];
 
-        const createdAt = data.map(item => item.createdAt);
+        const createdAt = data.map((item) => item.createdAt);
 
         const createChartData = (key) => {
-          const filteredData = data.filter(item => item[key] !== null);
-          const filteredCreatedAt = filteredData.map(item => item.createdAt);
+          const filteredData = data.filter((item) => item[key] !== null);
+          const filteredCreatedAt = filteredData.map((item) => item.createdAt);
 
           return {
             labels: filteredCreatedAt,
-            datasets: [{
-              label: key,
-              data: filteredData.map(item => item[key]),
-              fill: false,
-              backgroundColor: 'rgba(75, 192, 192, 0.4)',
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 1,
-            }],
+            datasets: [
+              {
+                label: key,
+                data: filteredData.map((item) => item[key]),
+                fill: false,
+                backgroundColor: "rgba(75, 192, 192, 0.4)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1,
+              },
+            ],
           };
         };
 
-        const generatedCharts = keys.map(key => ({
+        const generatedCharts = keys.map((key) => ({
           label: key,
-          data: createChartData(key)
+          data: createChartData(key),
         }));
 
         console.log("Generated Charts:", generatedCharts);
--- i-mirror/src/pages/analysis_result/AnalysisResult.jsx
+++ i-mirror/src/pages/analysis_result/AnalysisResult.jsx
@@ -5,8 +5,8 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import * as A from "./AnalysisResult.Style";
 import iMirrorLogo from "../../asset/img/i-mirror_logo.svg";
-<<<<<<< HEAD
 import useCheckup from "../../hooks/auth/useCheckup";
+import Chart from "../../components/Chart";
 
 const AnalysisResult = () => {
   const { koreanName, birthDate } = useParams();
@@ -57,7 +57,9 @@ const AnalysisResult = () => {
             <ResultTable patientData={patient} />
           </A.ResultTableContainer>
         </A.AnalysisResultPaper>
-        <A.AnalysisResultPaper>22</A.AnalysisResultPaper>
+        <A.AnalysisResultPaper>
+          <Chart name={patient.memberName} birth={patient.memberBirthDate} />
+        </A.AnalysisResultPaper>
       </A.AnalysisResultContainer>
     </A.AnalysisResultLayout>
   );
@@ -150,9 +152,6 @@ const ResultTable = ({ patientData }) => {
     </A.ResultTable>
   );
 };
-=======
-import Chart from "../../components/Chart";
->>>>>>> aee6777d5d8b9a48336e15690e56f30b065dadf5
 
 const Scene = () => {
   const { scene } = useGLTF("/models/MDA_man.glb");
@@ -165,12 +164,10 @@ const Scene = () => {
     }
   }, [scene]);
 
-  // 애니메이션
   useFrame(() => {
     gl.render(scene, camera);
   });
 
-  // 조명 설정
   return (
     <>
       <ambientLight />
@@ -184,7 +181,7 @@ const Scene = () => {
   );
 };
 
-// 여러 문제 부위(?) [ 서버에서 받은 데이터를 표준과 비교해서 차이가 심하다면 => 구체 표시 : 차이가 심하지 않다면 구체 표시X  ]
+// Spheres Component
 const Spheres = () => {
   const spheresData = [
     { position: [0, 0.6, -0.065], size: 0.05 }, // 목 틀어짐
@@ -192,10 +189,8 @@ const Spheres = () => {
     { position: [0.08, -0.45, 0.04], size: 0.075 }, // 오른쪽 무릎 틀어짐
     { position: [-0.15, 0.53, -0.04], size: 0.075 }, // 왼쪽 어깨 말림
     { position: [0.15, 0.53, -0.04], size: 0.075 }, // 오른쪽 어깨 말림
-    { position: [-0.15, 0, -0.02], size: 0.075 }, // 왼쪽 다리가 왼쪽보다 짧음
+    { position: [-0.15, 0, -0.02], size: 0.075 }, // 왼쪽 다리가 오른쪽보다 짧음
     { position: [0.15, 0, -0.02], size: 0.075 }, // 오른쪽 다리가 왼쪽보다 짧음
-    // { position: [-0.08, -0.85, -0.005], size: 0.065 }, // 왼쪽 발목 틀어짐
-    // { position: [0.08, -0.85, -0.005], size: 0.065 }, // 오른쪽 발목 틀어짐
   ];
 
   return spheresData.map((sphere, index) => (
@@ -206,9 +201,8 @@ const Spheres = () => {
   ));
 };
 
+// CanvasComponent
 const CanvasComponent = () => {
-  const canvasRef = useRef();
-
   return (
     <A.AvatarLayout>
       <A.AvatarContainer>
@@ -219,116 +213,3 @@ const CanvasComponent = () => {
     </A.AvatarLayout>
   );
 };
-
-<<<<<<< HEAD
-// 이제 여따가 서버에서 받은 데이터로 차트 처 만들껀데 그래프 하나에 다 때려 처박을까
-=======
-// ResultTable Component
-const ResultTable = () => {
-  const { koreanName } = useParams();
-  const patient = patientList.find(
-    (patient) => patient.koreanName === koreanName
-  );
-
-  if (!patient) {
-    return null;
-  }
-
-  return (
-    <A.ResultTable>
-      <A.ResultTr>
-        <A.ResultTd>1</A.ResultTd>
-        <A.ResultTd>목 틀어짐</A.ResultTd>
-        <A.ResultTd>앞으로 기울어짐</A.ResultTd>
-        <A.ResultTd>{patient.neckTwisted}</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>2</A.ResultTd>
-        <A.ResultTd>어깨 말림</A.ResultTd>
-        <A.ResultTd>앞으로 말림</A.ResultTd>
-        <A.ResultTd>왼 3.0 오 2.4</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>3</A.ResultTd>
-        <A.ResultTd>어깨 틀어짐</A.ResultTd>
-        <A.ResultTd>왼쪽 내려감</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>4</A.ResultTd>
-        <A.ResultTd>허리 틀어짐</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-        <A.ResultTd>20</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>5</A.ResultTd>
-        <A.ResultTd>골반 틀어짐</A.ResultTd>
-        <A.ResultTd>왼쪽다리가 긺</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>6</A.ResultTd>
-        <A.ResultTd>엉덩이 틀어짐</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-        <A.ResultTd>왼 3.0 오 2.4</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>7</A.ResultTd>
-        <A.ResultTd>무릎 틀어짐</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-        <A.ResultTd>왼 3.0 오 2.4</A.ResultTd>
-      </A.ResultTr>
-      <A.ResultTr>
-        <A.ResultTd>8</A.ResultTd>
-        <A.ResultTd>발목 틀어짐</A.ResultTd>
-        <A.ResultTd>-</A.ResultTd>
-        <A.ResultTd>왼 3.0 오 2.4</A.ResultTd>
-      </A.ResultTr>
-    </A.ResultTable>
-  );
-};
-
-// AnalysisResult Component
-const AnalysisResult = () => {
-  const { koreanName } = useParams();
-  const patient = patientList.find(
-    (patient) => patient.koreanName === koreanName
-  );
-
-  if (!patient) {
-    return <div>Patient not found</div>;
-  }
-
-  return (
-    <A.AnalysisResultLayout>
-      <A.AnalysisResultContainer>
-        <A.AnalysisResultPaper>
-          <A.AnalysisResultHeaderContainer>
-            <A.HeaderLogo src={iMirrorLogo} alt="logo" />
-            <A.HeaderTitle>Analysis Result</A.HeaderTitle>
-            <A.PatientInfoDiv>
-              <A.NameLabel>NAME: </A.NameLabel>
-              <A.PatientName>{patient.koreanName}</A.PatientName>
-              <A.BirthDateLabel>BIRTHDATE: </A.BirthDateLabel>
-              <A.PatientBirthDate>{patient.birthDate}</A.PatientBirthDate>
-              <A.GenderLabel>GENDER: </A.GenderLabel>
-              <A.PatientGender>{patient.gender}</A.PatientGender>
-              <A.PatientTestDate>{patient.testDate}</A.PatientTestDate>
-            </A.PatientInfoDiv>
-          </A.AnalysisResultHeaderContainer>
-          <A.LineDiv>
-            <A.Line />
-          </A.LineDiv>
-          <CanvasComponent />
-          <A.ResultTableContainer>
-            <ResultTable />
-          </A.ResultTableContainer>
-          <Chart name={patient.koreanName} birth={patient.birthDate} />
-        </A.AnalysisResultPaper>
-      </A.AnalysisResultContainer>
-    </A.AnalysisResultLayout>
-  );
-};
-
-export default AnalysisResult;
->>>>>>> aee6777d5d8b9a48336e15690e56f30b065dadf5
